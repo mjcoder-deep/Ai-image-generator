4870bbbd7688110ed9907851c2a458213205a2ab
Mijan <mntnupmz.apple@gmail.com>
2025-12-04 10:20:37 +0600

    Update app.js to use backend URL

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ async function generateImage() {
     document.getElementById("result").innerHTML = "<p>Generating...</p>";
 
     const response = await fetch(
-        "https://generativelanguage.googleapis.com/v1beta/models/imagetext:generate?key=" + API_KEY,
+        "http://localhost:3000/generate" + API_KEY,
         {
             method: "POST",
             headers: { "Content-Type": "application/json" },
